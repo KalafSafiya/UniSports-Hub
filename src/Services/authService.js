3d3e94b1b18789bb108df2f4e2e8615bc9caca1a
Chamara Wanasinghe <chamara.wanasinghe227@gmail.com>
2026-01-17 13:45:00 +0530

    Delete src directory

--- src/Services/authService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = "http://localhost:5000/api/auth";
-
-export const login = async (credentials) => {
-    const response = await axios.post(`${API_URL}/login`, credentials);
-    return response.data;
-}
